f43e35d288dc593ab257e5d60e59468462cc13ab
giannurwana19 <giannurwana19@gmail.com>
2021-08-13 20:10:26 +0700

    Membuat model

---
+++ src/models/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
