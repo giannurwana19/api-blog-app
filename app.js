b7d3579513875dfbada444d199b6c61f010ceae3
giannurwana19 <giannurwana19@gmail.com>
2021-08-14 21:08:05 +0700

    upload file

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const app = express();
 const port = 4000;
 
 dotenv.config();
 app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, 'hello.jpg');
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json({ message: 'File has been uploaded!' });
+});
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -26,6 +54,10 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/categories', categoryRoute);
+
+app.post('api/upload', (req, res) => {
+  res.status(200).json({ message: 'File has been uploaded!' });
+});
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${4000}`);
